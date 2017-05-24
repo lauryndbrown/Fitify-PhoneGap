cb9afc4168a327d167b4565c0a45db14c0d112e7
lauryndbrown <lauryndbrown@gmail.com>
2017-05-24 19:49:37 -0400

    Added a userfield to character. Added a user auth field.

--- fitifysite/fitify/api.py
+++ fitifysite/fitify/api.py
@@ -1,7 +1,9 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from fitify.models import Character
 
 class CharacterResource(ModelResource):
     class Meta:
         queryset = Character.objects.all()
         resource_name = 'character'
+        authorization = Authorization()
--- fitifysite/fitify/models.py
+++ fitifysite/fitify/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from tastypie.utils.timezone import now
+from django.contrib.auth.models import User
 
 class Character(models.Model):
+    user = models.ForeignKey(User)
     name = models.CharField(max_length=200)
     level = models.IntegerField(default=1)
