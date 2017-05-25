f34a246294ce63f3efaabc50ac080bd49cab0feb
lauryndbrown <lauryndbrown@gmail.com>
2017-05-24 20:03:06 -0400

    Added user resouces and used the api_v1 for registering resouces.

--- fitifysite/fitify/api.py
+++ fitifysite/fitify/api.py
@@ -1,8 +1,17 @@
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from fitify.models import Character
+from django.contrib.auth.models import User
+from tastypie import fields
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        allowed_methods = ['get']
 
 class CharacterResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
     class Meta:
         queryset = Character.objects.all()
         resource_name = 'character'
--- fitifysite/fitifysite/urls.py
+++ fitifysite/fitifysite/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from fitify.api import CharacterResource
-character_resource = CharacterResource()
+from fitify.api import CharacterResource, UserResource
+from tastypie.api import Api
+
+v1_api = Api(api_name='v1')
+v1_api.register(CharacterResource())
+v1_api.register(UserResource())
+
 urlpatterns = [
     url(r'^fitify/', include('fitify.urls')),
-    url(r'^api/', include(character_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', admin.site.urls),
 ]
