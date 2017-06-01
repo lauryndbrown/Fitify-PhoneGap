a25ed97ecd1845286ee364e239197230626f11ef
lauryndbrown <lauryndbrown@gmail.com>
2017-06-01 14:21:43 -0400

    refined the regex expression for getting the code for oauth.

--- www/js/oauth.js
+++ www/js/oauth.js
@@ -2,18 +2,17 @@ $(document).on('deviceready', function(){
     
     var authUrl = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=228J44&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
 
-   // var loginButton = $('#login a');
     var loginButton = $('#login-button');
     var loginStatus = $('#login-status');
     loginButton.click(function(){
         var authWindow = window.open(authUrl, "_blank");
         $( authWindow ).on('load',function(){
             var url = authWindow.location.href;
-            var code = /\?code=(.+)$/.exec(url);
+            var code = /\?code=(.+)#/.exec(url);
             var error = /\?error=(.+)$/.exec(url);
-            authWindow.close();
+            //authWindow.close();
             if(code) {
-                alert("code:"+code);
+                alert("code:"+code[1]);
                 $.post('https://api.fitbit.com/oauth2/token', {
                     code: code[1],
                     client_id: '228J44',
